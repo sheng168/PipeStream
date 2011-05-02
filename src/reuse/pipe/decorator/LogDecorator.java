8788ad000f5a811b2ac75c079dea2b37ca02e4c9
sheng <sheng@jsy.us>
2011-05-02 11:16:36 -0400

    make logging more flexible

--- src/reuse/pipe/decorator/LogDecorator.java
+++ src/reuse/pipe/decorator/LogDecorator.java
@@ -1,10 +1,25 @@
 package reuse.pipe.decorator;
 
+import org.slf4j.Logger;
+
 import reuse.pipe.Decorator;
 import reuse.pipe.Target;
 
 public class LogDecorator<T> extends Decorator<T> {
-	static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(LogDecorator.class);
+	private org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(LogDecorator.class);
+
+	private String logMsg = "{}";
+
+	public LogDecorator(Logger log, String logMsg, Target<T> target) {
+		super(target);
+		this.log = log;
+		this.logMsg = logMsg;
+	}
+
+	public LogDecorator(String logMsg, Target<T> target) {
+		super(target);
+		this.logMsg = logMsg;
+	}
 
 	public LogDecorator(Target<T> target) {
 		super(target);
@@ -12,7 +27,7 @@ public class LogDecorator<T> extends Decorator<T> {
 
 	@Override
 	public void send(T o) {
-		log.debug("{}", o);
+		log.debug(logMsg, o);
 		super.send(o);
 	}
 
