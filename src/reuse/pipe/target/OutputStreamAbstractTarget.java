a3c110995d43fddd6e12ed78c3768326adebc14f
sheng <sheng@jsy.us>
2011-05-04 17:35:13 -0400

    abstract class to provide constructor template

---
+++ src/reuse/pipe/target/OutputStreamAbstractTarget.java
@@ -0,0 +1,24 @@
+package reuse.pipe.target;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import reuse.pipe.Target;
+
+public class OutputStreamAbstractTarget<T> extends Target<T> implements Closeable{
+	static org.slf4j.Logger log = org.slf4j.LoggerFactory
+			.getLogger(OutputStreamAbstractTarget.class);
+	
+	private OutputStream os;
+
+	public OutputStreamAbstractTarget(OutputStream os) {
+		super();
+		this.os = os;
+	}
+
+	@Override
+	public void close() throws IOException {
+		os.close();
+	}
+}
