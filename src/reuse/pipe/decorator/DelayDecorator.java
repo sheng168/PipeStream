4cdc400b86ed20ad6a41ecc02c72fa9c0cb2fb18
sheng <sheng@jsy.us>
2011-05-05 12:18:51 -0400

    fix error where Thread.sleep doesn't accept large values in nanos param

--- src/reuse/pipe/decorator/DelayDecorator.java
+++ src/reuse/pipe/decorator/DelayDecorator.java
@@ -6,21 +6,24 @@ import reuse.pipe.Target;
 public class DelayDecorator<T> extends Decorator<T> {
 	static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(DelayDecorator.class);
 
-	private int nanos = 1000*1000; // 1ms
+	private long millis = 1;
+	private int nanos = 0;
 
 	public DelayDecorator(int nanos, Target<T> target) {
-		super(target);
-		this.nanos = nanos;
+		this(0, nanos, target);
 	}
 
-	public DelayDecorator(Target<T> target) {
+	public DelayDecorator(long millis, int nanos, Target<T> target) {
 		super(target);
+		this.millis = millis;
+		millis += nanos / 1000000;
+		this.nanos = nanos % 100000;
 	}
 
 	@Override
 	public void send(T o) {
 		try {
-			Thread.sleep(0, nanos);
+			Thread.sleep(millis, nanos);
 		} catch (InterruptedException e) {
 			log.warn("", e);
 		} finally {
