42bb46ddb4d919c97681a4080e13c2973cf83d4c
sheng <sheng@jsy.us>
2011-05-05 12:16:57 -0400

    expose object in JMX

---
+++ src/reuse/jmx/ObjectValue.java
@@ -0,0 +1,26 @@
+package reuse.jmx;
+
+
+public class ObjectValue implements ObjectValueMBean {
+	Object value;
+
+	public ObjectValue(String value, String objectName) {
+		this.value = value;
+		MBeanHelper.register(this, objectName);
+	}
+	
+	@Override
+	public Object getValue() {
+		return value;
+	}
+
+	@Override
+	public void setValue(Object value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return value.toString();
+	}
+}
